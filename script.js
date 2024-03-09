6998846ec5a6eb3ff405f896ca0cc2290fc9429f
ali <bovaqulov@gmail.com>
2024-03-09 16:15:00 +0500

    js

---
+++ script.js
@@ -0,0 +1,15 @@
+var a=0<=18
+var b=18<=30
+var c=30>=50
+var d=50>=70
+
+var misol = +prompt("18+17=")
+console.log( "18+17="+(18+17)+" Sizning javob "+misol);
+var misol = +prompt("51-27=")
+console.log( "51-27="+(51-27)+" Sizning javob "+misol);
+var misol = +prompt("25*55=")
+console.log( "25*55="+25*55+" Sizning javob "+misol);
+var misol = +prompt("128-64=")
+console.log( "128-64="+(128-64)+" Sizning javob "+misol);
+var misol = +prompt("256-128=")
+console.log( "256-128="+(256-128)+" Sizning javob "+misol);
